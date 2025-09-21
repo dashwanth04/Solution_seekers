50d97f321f50b7c2505eccaef12170821d3cd2e3
dashwanth_ram <ramchowdary640@gmail.com>
2025-09-21 10:31:51 +0530

    kk

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    fs: {
-      // Allow serving files from one level up to the project root
-      allow: ['..']
-    }
-  }
 })
